f6222327253b5c84c2f96c354e07c4254834a684
Julien Eudine <julien.eudine@telecom-paris.fr>
2020-08-20 09:50:23 +0200

    Formating improve by using Macros

--- tb_src/Tester.cpp
+++ tb_src/Tester.cpp
@@ -10,6 +10,16 @@
 #include <iostream>
 #include <cstdlib>
 
+#define GENERATION \
+    wait();\
+    counter ++;
+
+#define EXTRACTION \
+    trig = std::rand() % 2;\
+    wait();\
+    if (r_num_func != r_num_rtl && trig) nb_errors ++;
+
+
 void Tester::test() {
     unsigned int counter = 0;
     unsigned int nb_errors = 0;
@@ -23,53 +33,31 @@ void Tester::test() {
     std::cout << "The initialisation and the first generation start..." << std::endl;
     std::cout << "Seed: " << seed_v << std::endl;
 
-    while (!ready) {
-        wait();
-        counter ++;
-    }
+    while (!ready) { GENERATION }
+
     std::cout << "The initialisation and the first generation lasted " << counter << " periods." << std::endl;
     std::cout << "The first extraction stage starts..." << std::endl;
 
 
-    while (ready && !last) {
-        trig = std::rand() % 2;
-        wait();
-        if (r_num_func != r_num_rtl && trig)
-            nb_errors ++;
-    }
+    while (ready && !last) { EXTRACTION }
 
-    while (!trig) {
-        trig = std::rand() % 2;
-        wait();
-        if (r_num_func != r_num_rtl && trig)
-            nb_errors ++;
-    }
+    while (!trig) { EXTRACTION }
+
     std::cout << "All numbers have been output, the first extraction stage is over." << std::endl;
     trig = false;
     wait();
 
     counter = 0;
-    while (!ready) {
-        wait();
-        counter ++;
-    }
+    while (!ready) { GENERATION }
+
     std::cout << "The second generation starts..." << std::endl;
     std::cout << "The second generation lasted " << counter << " periods." << std::endl;
     std::cout << "The second extraction stage starts..." << std::endl;
 
-    while (ready && !last) {
-        trig = std::rand() % 2;
-        wait();
-        if (r_num_func != r_num_rtl && trig)
-            nb_errors ++;
-    }
+    while (ready && !last) { EXTRACTION }
 
-    while (!trig) {
-        trig = std::rand() % 2;
-        wait();
-        if (r_num_func != r_num_rtl && trig)
-            nb_errors ++;
-    }
+    while (!trig) { EXTRACTION }
+
     std::cout << "All numbers have been output, the second extraction stage is over." << std::endl;
     trig = false;
     wait(10);
